3fc548ce256fd8567de592de633566b40a8a9f85
Marian Keller <marian@laphtec.com>
2023-01-31 09:14:36 +0100

    PWA

--- cah-app/src/main.js
+++ cah-app/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
+import './registerServiceWorker'
 
 createApp(App).use(router).mount('#app')
